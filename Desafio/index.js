42330e98df4286952952b0d9306a89592af94b06
Lívia Santos <livia5153santos@gmail.com>
2023-12-15 11:03:53 -0300

    Comentando o desafio do código

---
+++ Desafio/index.js
@@ -0,0 +1,2 @@
+//Criando um código para armazenar o máximo de informações possíveis dos pokemons da imagem ao lado e ao final
+//para cada um, exibindo uma mensagem de saída escrita com o nome do pokemon concatenado com "Cadastrado com sucesso"
